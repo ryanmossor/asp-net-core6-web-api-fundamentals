fabfb1d3279b9b41e0b0ba961503ede7fc804ebd
Ryan Mossor <ryan.mossor@gmail.com>
2023-02-03 09:17:20 -0600

    Initial commit

---
+++ CityInfo.API/Program.cs
@@ -0,0 +1,31 @@
+﻿// We're building a web app which needs to be hosted. To do this, a WebApplicationBuilder can be used
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container (dependency injection)
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Hello world!");
+});
+
+app.Run();
+
